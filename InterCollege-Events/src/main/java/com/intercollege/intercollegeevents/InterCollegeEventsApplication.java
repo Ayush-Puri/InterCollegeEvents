430b68d28e16fc911d57071023ca75831f5de304
Ayush Puri <77268951+Ayush-Puri@users.noreply.github.com>
2024-11-19 18:36:35 +0530

    Added Events Service
    Primary service shell that displays all the available events a student can participate in based on college affiliations.

---
+++ InterCollege-Events/src/main/java/com/intercollege/intercollegeevents/InterCollegeEventsApplication.java
@@ -0,0 +1,13 @@
+package com.intercollege.intercollegeevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterCollegeEventsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterCollegeEventsApplication.class, args);
+    }
+
+}
---
+++ InterCollege-Events/src/test/java/com/intercollege/intercollegeevents/InterCollegeEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intercollege.intercollegeevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterCollegeEventsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
